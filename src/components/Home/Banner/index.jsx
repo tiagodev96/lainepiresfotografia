6836e3e2811c6605f196c8f39a96f311fdbbb134
Tiago Batista <tiago.dev96@gmail.com>
2023-03-25 13:43:09 -0300

    fix

--- src/components/Home/Banner/index.jsx
+++ src/components/Home/Banner/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BannerContainer, BannerContent, PictureLP, StyledH1 } from "./styles";
-import ProfilePictureLP from "../../../assets/homeImages/laine-picture.jpg";
+import ProfilePictureLP from "../../../assets/homeImages/lainePicture.jpg";
 
 const Banner = ({ roles }) => {
   const [rolesIndex, setRolesIndex] = useState(0);
