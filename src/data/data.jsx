13bff557b517770090586f4c1471998673fc6433
Tiago Batista <tiago.dev96@gmail.com>
2023-03-27 17:39:05 -0300

    NEW: Ensaio de Talita Fonoaudióloga

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -102,6 +102,22 @@ import TalitaEssay9 from "../assets/portfolio/thematic/aniversario-talita/9.webp
 import TalitaEssay9Thumb from "../assets/portfolio/thematic/aniversario-talita/9Thumb.webp";
 import TalitaEssay10 from "../assets/portfolio/thematic/aniversario-talita/10.webp";
 import TalitaEssay10Thumb from "../assets/portfolio/thematic/aniversario-talita/10Thumb.webp";
+import TalitaFonoEssay1 from "../assets/portfolio/corporate/talita/1.webp";
+import TalitaFonoEssay1Thumb from "../assets/portfolio/corporate/talita/1Thumb.webp";
+import TalitaFonoEssay2 from "../assets/portfolio/corporate/talita/2.webp";
+import TalitaFonoEssay2Thumb from "../assets/portfolio/corporate/talita/2Thumb.webp";
+import TalitaFonoEssay3 from "../assets/portfolio/corporate/talita/3.webp";
+import TalitaFonoEssay3Thumb from "../assets/portfolio/corporate/talita/3Thumb.webp";
+import TalitaFonoEssay4 from "../assets/portfolio/corporate/talita/4.webp";
+import TalitaFonoEssay4Thumb from "../assets/portfolio/corporate/talita/4Thumb.webp";
+import TalitaFonoEssay5 from "../assets/portfolio/corporate/talita/5.webp";
+import TalitaFonoEssay5Thumb from "../assets/portfolio/corporate/talita/5Thumb.webp";
+import TalitaFonoEssay6 from "../assets/portfolio/corporate/talita/6.webp";
+import TalitaFonoEssay6Thumb from "../assets/portfolio/corporate/talita/6Thumb.webp";
+import TalitaFonoEssay7 from "../assets/portfolio/corporate/talita/7.webp";
+import TalitaFonoEssay7Thumb from "../assets/portfolio/corporate/talita/7Thumb.webp";
+import TalitaFonoEssay8 from "../assets/portfolio/corporate/talita/8.webp";
+import TalitaFonoEssay8Thumb from "../assets/portfolio/corporate/talita/8Thumb.webp";
 
 // // NougConvida
 import NougConvidaEssay1 from "../assets/portfolio/event/noug-convida/2.webp";
@@ -469,6 +485,46 @@ export const dataBase = [
       {
         original: <RenataEssay10 />,
         thumbnail: <RenataEssay10Thumb />,
+      },
+    ],
+  },
+  {
+    id: 7,
+    src: <TalitaFonoEssay5 />,
+    category: "Corporativas",
+    description: "Fonoaudióloga",
+    images: [
+      {
+        original: <TalitaFonoEssay1 />,
+        thumbnail: <TalitaFonoEssay1Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay2 />,
+        thumbnail: <TalitaFonoEssay2Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay3 />,
+        thumbnail: <TalitaFonoEssay3Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay4 />,
+        thumbnail: <TalitaFonoEssay4Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay5 />,
+        thumbnail: <TalitaFonoEssay5Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay6 />,
+        thumbnail: <TalitaFonoEssay6Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay7 />,
+        thumbnail: <TalitaFonoEssay7Thumb />,
+      },
+      {
+        original: <TalitaFonoEssay8 />,
+        thumbnail: <TalitaFonoEssay8Thumb />,
       },
     ],
   },
