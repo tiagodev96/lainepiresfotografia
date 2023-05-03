680e3840af65bb59760dd56569210a7b6bbd3c2c
Tiago Batista <tiago.dev96@gmail.com>
2023-05-03 14:31:28 -0300

    Larissa Dermatologista uploaded

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -217,6 +217,7 @@ import EvaniEssay14Thumb from "../assets/portfolio/casual/evani/14Thumb.webp";
 import EvaniEssay15 from "../assets/portfolio/casual/evani/15.webp";
 import EvaniEssay15Thumb from "../assets/portfolio/casual/evani/15Thumb.webp";
 
+// Risa Rios Concept Evento
 import RisaRiosEssay1 from "../assets/portfolio/event/risa-rios-concept/1.webp";
 import RisaRiosEssay1Thumb from "../assets/portfolio/event/risa-rios-concept/1Thumbnail.webp";
 import RisaRiosEssay2 from "../assets/portfolio/event/risa-rios-concept/2.webp";
@@ -290,7 +291,95 @@ import RisaRiosEssay35Thumb from "../assets/portfolio/event/risa-rios-concept/35
 import RisaRiosEssay36 from "../assets/portfolio/event/risa-rios-concept/36.webp";
 import RisaRiosEssay36Thumb from "../assets/portfolio/event/risa-rios-concept/36Thumbnail.webp";
 
+// Larissa Dermatologista
+import LarissaEssay1 from "../assets/portfolio/corporate/larissa/1.webp";
+import LarissaEssay1Thumb from "../assets/portfolio/corporate/larissa/1Thumbnail.webp";
+import LarissaEssay2 from "../assets/portfolio/corporate/larissa/2.webp";
+import LarissaEssay2Thumb from "../assets/portfolio/corporate/larissa/2Thumbnail.webp";
+import LarissaEssay3 from "../assets/portfolio/corporate/larissa/3.webp";
+import LarissaEssay3Thumb from "../assets/portfolio/corporate/larissa/3Thumbnail.webp";
+import LarissaEssay4 from "../assets/portfolio/corporate/larissa/4.webp";
+import LarissaEssay4Thumb from "../assets/portfolio/corporate/larissa/4Thumbnail.webp";
+import LarissaEssay5 from "../assets/portfolio/corporate/larissa/5.webp";
+import LarissaEssay5Thumb from "../assets/portfolio/corporate/larissa/5Thumbnail.webp";
+import LarissaEssay6 from "../assets/portfolio/corporate/larissa/6.webp";
+import LarissaEssay6Thumb from "../assets/portfolio/corporate/larissa/6Thumbnail.webp";
+import LarissaEssay7 from "../assets/portfolio/corporate/larissa/7.webp";
+import LarissaEssay7Thumb from "../assets/portfolio/corporate/larissa/7Thumbnail.webp";
+import LarissaEssay8 from "../assets/portfolio/corporate/larissa/8.webp";
+import LarissaEssay8Thumb from "../assets/portfolio/corporate/larissa/8Thumbnail.webp";
+import LarissaEssay9 from "../assets/portfolio/corporate/larissa/9.webp";
+import LarissaEssay9Thumb from "../assets/portfolio/corporate/larissa/9Thumbnail.webp";
+import LarissaEssay10 from "../assets/portfolio/corporate/larissa/10.webp";
+import LarissaEssay10Thumb from "../assets/portfolio/corporate/larissa/10Thumbnail.webp";
+import LarissaEssay11 from "../assets/portfolio/corporate/larissa/11.webp";
+import LarissaEssay11Thumb from "../assets/portfolio/corporate/larissa/11Thumbnail.webp";
+import LarissaEssay12 from "../assets/portfolio/corporate/larissa/12.webp";
+import LarissaEssay12Thumb from "../assets/portfolio/corporate/larissa/12Thumbnail.webp";
+import LarissaEssay13 from "../assets/portfolio/corporate/larissa/13.webp";
+import LarissaEssay13Thumb from "../assets/portfolio/corporate/larissa/13Thumbnail.webp";
+
 export const dataBase = [
+  {
+    id: 10,
+    src: <LarissaEssay1 />,
+    category: "Corporativas",
+    description: "Dermatologista",
+    images: [
+      {
+        original: <LarissaEssay1 />,
+        thumbnail: <LarissaEssay1Thumb />,
+      },
+      {
+        original: <LarissaEssay2 />,
+        thumbnail: <LarissaEssay2Thumb />,
+      },
+      {
+        original: <LarissaEssay3 />,
+        thumbnail: <LarissaEssay3Thumb />,
+      },
+      {
+        original: <LarissaEssay4 />,
+        thumbnail: <LarissaEssay4Thumb />,
+      },
+      {
+        original: <LarissaEssay5 />,
+        thumbnail: <LarissaEssay5Thumb />,
+      },
+      {
+        original: <LarissaEssay6 />,
+        thumbnail: <LarissaEssay6Thumb />,
+      },
+      {
+        original: <LarissaEssay7 />,
+        thumbnail: <LarissaEssay7Thumb />,
+      },
+      {
+        original: <LarissaEssay8 />,
+        thumbnail: <LarissaEssay8Thumb />,
+      },
+      {
+        original: <LarissaEssay9 />,
+        thumbnail: <LarissaEssay9Thumb />,
+      },
+      {
+        original: <LarissaEssay10 />,
+        thumbnail: <LarissaEssay10Thumb />,
+      },
+      {
+        original: <LarissaEssay11 />,
+        thumbnail: <LarissaEssay11Thumb />,
+      },
+      {
+        original: <LarissaEssay12 />,
+        thumbnail: <LarissaEssay12Thumb />,
+      },
+      {
+        original: <LarissaEssay13 />,
+        thumbnail: <LarissaEssay13Thumb />,
+      },
+    ],
+  },
   {
     id: 9,
     src: <RisaRiosEssay1 />,
